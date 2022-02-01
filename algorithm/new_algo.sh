eaaf012e0b98c92d2288ab3fe80a5d5b0c0c41d3
is-yeop <yeop@yeop.kr>
2022-02-01 19:44:51 +0900

    Add space in Readme title\n\n# 이후에 공백이 없으면 title로 취급하지 않는 오류 해결

--- algorithm/new_algo.sh
+++ algorithm/new_algo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir "$1" && cd $_;
 touch "$PWD/main.cpp"
-echo "#[$2](https://acmicpc.net/problem/$1)"> "$PWD/README.md"
+echo "# [$2](https://acmicpc.net/problem/$1)"> "$PWD/README.md"
 clion "$PWD/main.cpp"
 clion "$PWD/README.md"
 open "https://acmicpc.net/problem/$1"
